d570ffdc12aab96ae61c73baf276ce02ef0e1d45
Kasztovszky Mihály <kasztovszky.mihaly@gmail.com>
2025-03-05 20:46:48 +0100

    setting up class members and precalculations

--- 2D_Eigenfluid/Assets/Scripts/EigenfluidRenderer.cs
+++ 2D_Eigenfluid/Assets/Scripts/EigenfluidRenderer.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EigenfluidRenderer : MonoBehaviour
@@ -8,60 +10,110 @@ public class EigenfluidRenderer : MonoBehaviour
     private int width;
     private int height; 
 
-    private readonly int dimension = 5;
-    private Vector3[] velocityCoeff;
-    private Vector3[] voricityCoeff;
-    private float[] eigenValues;
-    private float[,,] sturctCoeffMatrixes;
+    private readonly int N = 16;
+    private int sqrtN; 
+    private readonly Dictionary<Vector2, int> reverseWaveNumberLookup = new();
+    private readonly Dictionary<int, Vector2> waveNumberLookup = new();
 
-    private Vector3 xAxis = new(1.0f, 0.0f, 0.0f);
-    private Vector3 yAxis = new(0.0f, 1.0f, 0.0f);
-    private Vector3 zAxis = new(0.0f, 0.0f, 1.0f);
+    private Vector2[,,] velocityCoeff; //--
+    private float[] eigenValues; //--
+    private float[,,] sturctCoeffMatrixes;
+    
 
     void Start()
     {
+        
+        this.InitializeTexture();
+
         this.Initialize();
 
-        spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
-
-        width = Screen.width;
-        height = Screen.height;
-
-        // Create a RenderTexture for compute shader
-        renderTexture = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32);
-        renderTexture.enableRandomWrite = true;
-        renderTexture.Create();
-
         // Dispatch Compute Shader
-        computeShader.SetTexture(0, "Result", renderTexture);
-        int threadGroupsX = Mathf.CeilToInt(width / 8.0f);
-        int threadGroupsY = Mathf.CeilToInt(height / 8.0f);
+        computeShader.SetTexture(0, "Result", this.renderTexture);
+        int threadGroupsX = Mathf.CeilToInt(this.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(this.height / 8.0f);
         computeShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
 
         // Convert RenderTexture to Sprite
-        Texture2D texture2D = new Texture2D(width, height, TextureFormat.RGBA32, false);
+        Texture2D texture2D = new Texture2D(this.width, this.height, TextureFormat.RGBA32, false);
         RenderTexture.active = renderTexture;
-        texture2D.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+        texture2D.ReadPixels(new Rect(0, 0, this.width, this.height), 0, 0);
         texture2D.Apply();
         RenderTexture.active = null;
 
         // Assign the texture to a sprite
-        spriteRenderer.sprite = Sprite.Create(texture2D, new Rect(0, 0, width, height), new Vector2(0.5f, 0.5f));
+        this.spriteRenderer.sprite = Sprite.Create(texture2D, new Rect(0, 0, width, height), new Vector2(0.5f, 0.5f));
     }
 
     void OnDestroy()
     {
-        renderTexture.Release();
+        this.renderTexture.Release();
     }
 
     private void Initialize(){
-        velocityCoeff = new Vector3[dimension];
-        voricityCoeff = new Vector3[dimension];
-        eigenValues = new float[dimension];
-        //TODO compute coefficients
-        
-        sturctCoeffMatrixes = new float[dimension,dimension,dimension];
+        this.sqrtN = (int)Math.Sqrt((double)this.N);
+        this.FillLookupTables();
+
+        eigenValues = new float[this.N];
+        this.FillEigenValues();
+
+        velocityCoeff = new Vector2[this.N,this.width,this.height];
+        this.PrecomputeVelocityCoeffs();        
+
+
+        sturctCoeffMatrixes = new float[this.N,this.N,this.N];
         //TODO precompute sturctCoeffMatrix
 
+    }
+
+    private void PrecomputeVelocityCoeffs()
+    {
+        Vector2 vel = new();
+        for(int i = 0; i< this.width; i++){
+            for(int j = 0; j< this.height;j++){
+                for(int k = 1; k <= this.N; k++){
+                    Vector2 k1k2 = waveNumberLookup[k];
+                    
+                    float x = i*((float)Math.PI/this.width);
+                    float y = j*((float)Math.PI/this.height);
+
+                    float denominator = 1.0f/(k1k2.x*k1k2.x + k1k2.y*k1k2.y);
+                    vel.x = denominator *  (k1k2.y * MathF.Sin(k1k2.x * x) * MathF.Cos(k1k2.y * y));
+                    vel.y = denominator *  -1.0f * (k1k2.x * MathF.Cos(k1k2.x * x) * MathF.Sin(k1k2.y * y));
+                    
+                    velocityCoeff[k-1,i,j] = vel;
+                }
+            }
+        }
+    }
+
+    private void FillLookupTables(){
+        int k = 1;
+        for(int i = 1; i<=this.sqrtN; i++){
+            for(int j = 1; j<=this.sqrtN; j++){
+                Vector2 k1k2 = new Vector2(i, j);
+                reverseWaveNumberLookup.Add(k1k2, k);
+                waveNumberLookup.Add(k, k1k2);
+                k++;
+            }
+        }
+    }
+
+    private void FillEigenValues(){
+        for(int k = 1; k <= this.N; ){
+            Vector2 k1k2 = waveNumberLookup[k];
+            eigenValues[k-1] = -1.0f* (k1k2.x*k1k2.x + k1k2.y*k1k2.y);
+        }
+    }
+
+    private void InitializeTexture(){
+        this.spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+
+        this.width = Screen.width;
+        this.height = Screen.height;
+
+        // Create a RenderTexture for compute shader
+        renderTexture = new RenderTexture(this.width, this.height, 0, RenderTextureFormat.ARGB32);
+        renderTexture.enableRandomWrite = true;
+        renderTexture.Create();
     }
 }

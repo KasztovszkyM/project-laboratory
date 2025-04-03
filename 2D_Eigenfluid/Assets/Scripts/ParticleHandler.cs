ec954caa7eae487d49ecdd812782b9a3c097c52f
Kasztovszky Mihály <kasztovszky.mihaly@gmail.com>
2025-04-03 11:05:53 +0200

    particles follow speed somewhat accurately

--- 2D_Eigenfluid/Assets/Scripts/ParticleHandler.cs
+++ 2D_Eigenfluid/Assets/Scripts/ParticleHandler.cs
@@ -30,12 +30,15 @@ public class ParticleHandler : MonoBehaviour
         Vector2[] positions = new Vector2[nParticles];
         for(int i = 0; i < nParticles; i++){
             //Debug.Log(positions[i]);
-            particles[i].RecalculatePos(CalculateVelocity(particles[i].GetPosition()));
+            particles[i].RecalculatePos(CalculateVelocity(particles[i].GetPosition()), Renderer.timeStep);
             positions[i] = particles[i].GetPosition();
         }
         particleBuffer.SetData(positions);
         particleMaterial.SetBuffer("_ParticlePositions",particleBuffer);
         particleMaterial.SetInt("_nParticles", nParticles);
+
+        particleMaterial.SetPass(0);
+        Graphics.DrawProceduralNow(MeshTopology.Points, nParticles);        
     }
 
     void OnRenderObject()
@@ -55,15 +58,21 @@ public class ParticleHandler : MonoBehaviour
         System.Random random = new();
         for(int i = 0; i<nParticles; i++){
            particles[i] = new Particle(new Vector2(UnityEngine.Random.Range(-1.0f, 1.0f), UnityEngine.Random.Range(-1.0f, 1.0f)));
+           Debug.Log(particles[i]);
         }
     }
 
     private Vector2 CalculateVelocity(Vector2 pos){
-        Vector2 velocity = new(0.0f,0.0f);
-        for(int k = 0; k < Renderer.N; k++){
-            velocity += Renderer.GetCoefs()[k] * Renderer.GetEigenFunctions()[k, (int)pos.x * width, (int)pos.y * height];
+        Vector2 velocity = new(0.0f, 0.0f);
+        Vector2 gridPosition = new ((pos.x+1.0f)*width/2.0f , (pos.y+1.0f)*height/2.0f);
+        if(gridPosition.x<= Renderer.width && gridPosition.y <= Renderer.height && gridPosition.x >= 0 && gridPosition.y >= 0){
+            for(int k = 0; k < Renderer.N; k++){
+                velocity += Renderer.GetCoefs()[k] * Renderer.GetEigenFunctions()[k, (int)gridPosition.x , (int)gridPosition.y];
+            }
         }
+        Debug.Log(velocity);
         return velocity;
+        
     }
 }
 
@@ -74,9 +83,9 @@ public class Particle{
     public Particle(Vector2 pos){
         this.position = pos;
     }
-    public void RecalculatePos(Vector2 vel){
+    public void RecalculatePos(Vector2 vel, float dt){
         this.velocity = vel;
-        this.position += velocity;
+        this.position += velocity * dt;
     }
 
     public Vector2 GetPosition(){
